e1c00a01a738067922ce4a6e36d0d96cbdb08ac7
luukberkers@protonmail.com <luukberkers@protonmail.com>
2022-12-10 13:04:21 +0100

    Partial algebra

--- src/Algebra.hs
+++ src/Algebra.hs
@@ -3,7 +3,26 @@ module Algebra where
 import           Model
 
 -- Exercise 5
-type Algebra = ()
+type Algebra program rule cmd dir alt pat
+    = ( -- Program
+        [rule] -> program
+      , -- Rule
+        String -> [cmd] -> rule
+      , -- Go
+        cmd
+      , -- Take
+        cmd
+      , -- Mark
+        cmd
+      , -- Not
+        cmd
+      , -- Turn
+        dir -> cmd
+      , -- Case
+        dir -> [alt] -> cmd
+      , -- Ident
+        String -> cmd
+      )
 
 fold = undefined
 
